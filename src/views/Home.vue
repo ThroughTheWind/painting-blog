d8bae0d7ec101c4c966cb691adf1a7ebb53e42b6
Antoine Dcr <antoine.dcr@outlook.fr>
2019-01-29 08:29:51 -0800

    Init Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,45 @@
 <template>
   <v-app dark>
-    <v-toolbar app flat>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>Let's Try this Framework !</v-toolbar-title>
+    <v-navigation-drawer v-model="drawer" app fixed>
+       <v-toolbar flat >
+        <v-list class="pa-0">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>mdi-apps</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Les applications</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-list>
+        <v-list-tile 
+          v-for="appDesc in appDescriptions" 
+          :key="appDesc.id"
+          :to="{path: '/'+appDesc.routerLink}"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ appDesc.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ appDesc.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app fixed flat>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
-        <v-btn flat>Link Two</v-btn>
-        <v-btn flat>Link Three</v-btn>
+        <v-btn v-for="appDesc in appDescriptions" :key="appDesc.id" :to="{path: '/'+appDesc.routerLink}">
+          {{ appDesc.title }} <v-icon>{{appDesc.icon}}</v-icon>
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
-      <v-container fluid>
-        
-      </v-container>
+      <hello-world />
     </v-content>
     <v-footer app></v-footer>
   </v-app>
@@ -21,14 +47,42 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/sections/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
   },
+
 })
 export default class Home extends Vue {
-  
+  title: string = 'Let\'s Try this Framework !';
+  drawer :boolean = false;
+  appDescriptions: any[] = [
+  {
+    id: 0,
+    title: 'Home',
+    description: 'Nothing to say right now.',
+    routerLink: '',
+    icon: 'mdi-home'
+  },{
+    id: 1,
+    title: 'Blog App',
+    description: 'Nothing to say right now.',
+    routerLink: 'blog',
+    icon: 'mdi-open-in-new'
+  },{
+    id: 2,
+    title: 'Admin App',
+    description: 'Nothing to say right now.',
+    routerLink: 'admin',
+    icon: 'mdi-open-in-new'
+  },{
+    id: 3,
+    title: 'Demo App',
+    description: 'Nothing to say right now.',
+    routerLink: 'demo',
+    icon: 'mdi-open-in-new'
+  },]
 }
 </script>
